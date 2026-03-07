0b21799614edbc305a1e6a4673f3314972ad1976
BloodForg <ruslan_421@mail.ru>
2026-03-07 11:44:22 +0700

    release: bump 1.3.1 and disable signing in mac workflow

--- src/App.tsx
+++ src/App.tsx
@@ -11,7 +11,7 @@ import { networkService } from './services/networkService';
 import { updateService } from './services/updateService';
 import { selectSelectedServer, useLauncherStore } from './store/useLauncherStore';
 
-const APP_VERSION = '1.3.0';
+const APP_VERSION = '1.3.1';
 
 function App() {
   const {
